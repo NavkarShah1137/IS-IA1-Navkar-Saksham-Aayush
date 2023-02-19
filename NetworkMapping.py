15a486d4aa96bc4e2b2555b9ee7bd3a1d7c6ad57
Navkar Shah <84144411+NavkarShah1137@users.noreply.github.com>
2023-02-19 12:23:17 +0530

    Add files via upload

---
+++ NetworkMapping.py
@@ -0,0 +1,14 @@
+import nmap
+
+# Prompt user for subnet to scan
+subnet = input("Enter subnet to scan: ")
+
+# Create a new nmap scanner object
+nm = nmap.PortScanner()
+
+# Perform network mapping scan on the specified subnet
+nm.scan(hosts=subnet, arguments='-sn')
+
+# Print out the list of hosts that were found
+for host in nm.all_hosts():
+    print('Host found: %s (%s)' % (host, nm[host].hostname()))
